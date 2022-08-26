18656a69e7999c6552343e1d95b7c22c07a5698b
B1G-M0N3Y <noahkerner843@gmail.com>
2022-08-26 17:11:45 -0600

    capitalized port

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,7 +1,7 @@
 
 module.exports = {
     environment: process.env.NODE_ENV || 'development',
-    port: process.env.port || 8000,
+    port: process.env.PORT || 8000,
     dbFile: process.env.DB_FILE,
     jwtConfig: {
         secret: process.env.JWT_SECRET,
