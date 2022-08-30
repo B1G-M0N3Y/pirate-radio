f1ded29bcea399469351e053acb888cbfc06dbbd
B1G-M0N3Y <noahkerner843@gmail.com>
2022-08-30 15:51:42 -0600

    added error message if the req to songs/:id has invalid id

--- backend/routes/api/songs.js
+++ backend/routes/api/songs.js
@@ -20,19 +20,30 @@ router.get(
 router.get(
     '/:id',
     async (req, res) => {
-        const songs = await Song.findByPk(req.params.id, {
-            include: [{
-                model: User,
-                as: 'Artist',
-                attributes: ['id', 'username', 'imageUrl']
-            },
-            {
-                model: Album,
-                attributes: ['id', 'title', 'imageUrl']
-            }]
+        const id = req.params.id;
+        if (id <= 0 || id > await Song.count()) {
+            res.status(404);
+            res.send({
+                "message": "Song couldn't be found",
+                "statusCode": 404
+            });
+        } else {
+
+            const songs = await Song.findByPk(req.params.id, {
+                include: [{
+                    model: User,
+                    as: 'Artist',
+                    attributes: ['id', 'username', 'imageUrl']
+                },
+                {
+                    model: Album,
+                    attributes: ['id', 'title', 'imageUrl']
+                }]
+            }
+            );
+
+            return res.json(songs)
         }
-        );
-        return res.json(songs)
     }
 )
 
