b707a4e9b84dca98a41958d107a173c4a9d818b7
B1G-M0N3Y <noahkerner843@gmail.com>
2022-08-30 14:38:51 -0600

    implemented GET /current backend endpoint

--- backend/routes/api/songs.js
+++ backend/routes/api/songs.js
@@ -1,14 +1,27 @@
 const express = require('express');
 
 const { Song } = require('../../db/models')
+const { restoreUser } = require('../../utils/auth')
 
 const router = express.Router();
 
 router.get(
+    '/current',
+    restoreUser,
+    async (req, res) => {
+        const { user } = req;
+        const songs = await Song.findAll({
+            where: {userId: user.toSafeObject().id}
+        });
+        return res.json({ songs });
+    }
+);
+
+router.get(
     '/',
-    async(req, res) => {
+    async (req, res) => {
         const songs = await Song.findAll();
-        return res.json({songs})
+        return res.json({ songs });
     }
 );
 
