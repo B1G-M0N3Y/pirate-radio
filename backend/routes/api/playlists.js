dcaae3668811254bca0443dd2269f6d12334b8f6
B1G-M0N3Y <noahkerner843@gmail.com>
2022-09-02 12:02:17 -0600

    POST playlists/:id/songs backend endpoint complete

--- backend/routes/api/playlists.js
+++ backend/routes/api/playlists.js
@@ -1,7 +1,7 @@
 const express = require('express');
 // const { route } = require('express/lib/router');
 
-const { Song, User, Album, Comment, Playlist } = require('../../db/models')
+const { Song, User, Album, Comment, Playlist, PlaylistSong } = require('../../db/models')
 const { restoreUser } = require('../../utils/auth')
 
 const router = express.Router();
@@ -70,6 +70,41 @@ router.get(
     }
 );
 
+router.post(
+    '/:id/songs',
+    restoreUser,
+    async (req, res) => {
+        const { user } = req;
+        const { songId } = req.body;
+        const id = req.params.id;
+
+        if(!(await Playlist.findByPk(id))){
+            res.status(404);
+            return res.send({
+                "message": "Playlist couldn't be found",
+                "statusCode": 404
+            });
+        }
+
+        if (!(await Song.findByPk(songId))) {
+            res.status(404);
+            return res.send({
+                "message": "Song couldn't be found",
+                "statusCode": 404
+            });
+        }
+
+        const greatestVal = await PlaylistSong.max('order', { where: {playlistId: id}});
+
+        const playlistSong = await PlaylistSong.create({
+            songId,
+            playlistId: id,
+            order: greatestVal ? greatestVal.order + 1 : 1
+        });
+
+        res.json(playlistSong);
+    })
+
 router.post('/',
     restoreUser,
     async (req, res) => {
@@ -92,6 +127,7 @@ router.post('/',
             imageUrl
         });
 
+        res.status(201);
         res.json(newPlaylist);
     }
 );
