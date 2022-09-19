c3dd675d08631f763779e4ceda514158861fc60c
B1G-M0N3Y <noahkerner843@gmail.com>
2022-09-19 15:12:59 -0600

    removed unneccsary .user

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -26,7 +26,7 @@ export const login = (user) => async (dispatch) => {
         }),
     });
     const data = await response.json();
-    dispatch(setUser(data.user));
+    dispatch(setUser(data));
     return response;
 };
 
@@ -44,14 +44,14 @@ export const signup = (user) => async (dispatch) => {
         }),
     });
     const data = await response.json();
-    dispatch(setUser(data.user));
+    dispatch(setUser(data));
     return response;
 };
 
 export const restoreUser = () => async dispatch => {
     const response = await csrfFetch('/api/session');
     const data = await response.json();
-    dispatch(setUser(data.user));
+    dispatch(setUser(data));
     return response;
 };
 
