86407274f1d62170b4c88df4eb933776291e2176
B1G-M0N3Y <noahkerner843@gmail.com>
2022-09-19 09:24:41 -0600

    setup frontend for heroku deployment

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -38,10 +38,6 @@ if (process.env.NODE_ENV !== 'production') {
   });
 }
 
-// router.get('/hello/world', function(req, res) {
-//   res.cookie('XSRF-TOKEN', req.csrfToken());
-//   res.send('Hello World!');
-// });
 router.get("/api/csrf/restore", (req, res) => {
     const csrfToken = req.csrfToken();
     res.cookie("XSRF-TOKEN", csrfToken);
