a34da835911d1bd88298ab9a0c1d5f0eff917d0b
B1G-M0N3Y <noahkerner843@gmail.com>
2022-09-22 16:04:24 -0600

    audio player plays selected song

--- backend/db/seeders/20220829235021-song.js
+++ backend/db/seeders/20220829235021-song.js
@@ -19,16 +19,16 @@ module.exports = {
         userId: 1,
         title: 'Brand New Shiny Nickle',
         description: 'Nickelback is back with a brand new nickle',
-        url: 'https://www.youtube.com/watch?v=CD8nrL8ttNI',
-        imageUrl: 'funnpic.xD'
+        url: 'https://res.cloudinary.com/dy199z8qt/video/upload/v1663882745/Nickelback_-_How_You_Remind_Me_OFFICIAL_VIDEO_qj2hg6.mp3',
+        imageUrl: 'https://res.cloudinary.com/dy199z8qt/image/upload/v1663882070/5c4ae7e66cf99614a868eb5f-large_ozqkpu.webp'
       },
       {
         albumId: 2,
         userId: 2,
         title: 'The Missle Knows Where it is',
         description: 'The missle knows where it is because the missle knows where it isnt',
-        url: 'https://www.youtube.com/watch?v=bZe5J8SVCYQ',
-        imageUrl: 'missle.jpg'
+        url: 'https://res.cloudinary.com/dy199z8qt/video/upload/v1663883129/The_Missile_Knows_Where_It_Is..._eypsmb.mp4',
+        imageUrl: 'https://res.cloudinary.com/dy199z8qt/image/upload/v1663883428/hypersonic_tjynyw.jpg'
       },
       {
         albumId: 3,
