333082a330513806027c500a3103ced726e7f2d3
B1G-M0N3Y <noahkerner843@gmail.com>
2022-10-11 13:52:46 -0600

    refactored SongCard into seperate component

---
+++ frontend/src/components/Songs/SongCard.js
@@ -0,0 +1,26 @@
+import { useDispatch } from "react-redux";
+import { NavLink } from "react-router-dom";
+import { fetchAudioDetails } from "../../store/audioPlayer";
+
+const SongCard = ({ song }) => {
+  const dispatch = useDispatch();
+
+  const changeSong = async (id) => {
+    await dispatch(fetchAudioDetails(id));
+  };
+
+  return (
+    <div className="song-card" onClick={() => changeSong(song.id)}>
+      <img className="song-pic" src={song.imageUrl} />
+      <div className="play-song">
+        <img src="https://res.cloudinary.com/dy199z8qt/image/upload/v1663887398/songplay_tb28tn.png" />
+      </div>
+      <NavLink key={song.id} className="song-link" to={`/songs/${song.id}`}>
+        <p className="song-title">{song.title}</p>
+        <p className="song-user">{song.Artist?.username}</p>
+      </NavLink>
+    </div>
+  );
+};
+
+export default SongCard;
