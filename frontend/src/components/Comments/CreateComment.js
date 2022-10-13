9aeab89c710dbe885cf285c6fd71aee26f6c6ee4
B1G-M0N3Y <noahkerner843@gmail.com>
2022-10-13 16:53:19 -0600

    fixed error in create comment allowing blank comments after refresh

--- frontend/src/components/Comments/CreateComment.js
+++ frontend/src/components/Comments/CreateComment.js
@@ -1,52 +1,51 @@
 import { useState } from "react";
-import { useDispatch } from "react-redux"
+import { useDispatch } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
 import { createNewComment } from "../../store/comments";
 import { createNewSong } from "../../store/songs";
 
 const CreateComment = () => {
-    const { id } = useParams()
-    const dispatch = useDispatch();
-    const history = useHistory();
-    const [commentBody, setCommentBody] = useState("");
-    const [validationError, setValidationError] = useState('')
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const [commentBody, setCommentBody] = useState("");
+  const [validationError, setValidationError] = useState("");
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        if(!commentBody) {
-            setValidationError("You must write a comment before posting");
-        }
+    if (!commentBody) {
+      setValidationError("You must write a comment before posting");
+    } else {
+      const payload = {
+        body: commentBody,
+      };
 
-        const payload = {
-            body: commentBody
-        }
+      const comment = await dispatch(createNewComment(payload, id));
 
-        const comment = await dispatch(createNewComment(payload, id));
+      if (comment) {
+        history.push(`/songs/${id}`);
+      }
 
-        if (comment) {
-            history.push(`/songs/${id}`)
-        }
-
-        return comment;
+      return comment;
     }
+  };
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <div className = "errors">
-                <p>{validationError}</p>
-            </div>
-            <label>
-                Leave a Comment:
-                <textarea
-                    value={commentBody}
-                    onChange={(e) => setCommentBody(e.target.value)}>
-
-                </textarea>
-            </label>
-            <button type='submit'>Post Comment</button>
-        </form>
-    )
-}
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="errors">
+        <p>{validationError}</p>
+      </div>
+      <label>
+        Leave a Comment:
+        <textarea
+          value={commentBody}
+          onChange={(e) => setCommentBody(e.target.value)}
+        ></textarea>
+      </label>
+      <button type="submit">Post Comment</button>
+    </form>
+  );
+};
 
 export default CreateComment;
