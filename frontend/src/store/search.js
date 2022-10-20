741c825df648daf57a6969ce331c14f1afebb4e8
B1G-M0N3Y <noahkerner843@gmail.com>
2022-10-20 10:07:24 -0600

    added thunk,action,reducer to search

---
+++ frontend/src/store/search.js
@@ -0,0 +1,36 @@
+const SEARCH_RESULTS = "search-results/";
+
+const searchResults = (results) => {
+  return {
+    type: SEARCH_RESULTS,
+    results,
+  };
+};
+
+export const fetchSearchResults = (query) => async (dispatch) => {
+  const songs = await fetch(`/api/songs?search=${query}`);
+  const artists = await fetch(`/api/artists?search=${query}`);
+
+  if (songs.ok && artists.ok) {
+    const songsJSON = await songs.json();
+    const artistsJSON = await artists.json();
+
+    const results = { songs: { ...songsJSON }, artists: { ...artistsJSON } };
+
+    dispatch(searchResults(results));
+    return results;
+  }
+};
+
+const initialState = {};
+
+const searchReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SEARCH_RESULTS:
+      return { ...action.results };
+    default:
+      return state;
+  }
+};
+
+export default searchReducer;
