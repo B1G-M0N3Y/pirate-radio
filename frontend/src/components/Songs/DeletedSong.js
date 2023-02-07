518c292a0dc901c70d8372af9e7cd3dc72040383
B1G-M0N3Y <noahkerner843@gmail.com>
2023-02-07 14:28:59 -0600

    changed jolly roger to aws image

--- frontend/src/components/Songs/DeletedSong.js
+++ frontend/src/components/Songs/DeletedSong.js
@@ -1,12 +1,17 @@
 import './DeletedSong.css'
 import { Link } from 'react-router-dom';
 
+const JOLLY_OGER_IMG_URL = "https://pirate-radio-kerner.s3.us-east-2.amazonaws.com/293956_uclywx.svg"
+
 const DeletedSong = () => {
     return (
         <div className='deleted-container'>
             <div className="deleted">
                 <h2>Song deleted!</h2>
-                <img className="jolly-roger" src='https://res.cloudinary.com/dy199z8qt/image/upload/v1664168790/293956_uclywx.svg'></img>
+                <img
+                    className="jolly-roger"
+                    src={JOLLY_OGER_IMG_URL}
+                    alt='Jolly Roger'></img>
                 <Link to='/songs'>
                     <button className='plunder-button'>Plunder for More</button>
                 </Link>
